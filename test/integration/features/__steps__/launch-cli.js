ac71c4b46aece4bcc827673b9518605545f9c1db
Jamie Greeff <jamiegreeff@gmail.com>
2016-04-13 15:38:01 +0100

    Add integration tests for launch CLI

---
+++ test/integration/features/__steps__/launch-cli.js
@@ -0,0 +1,22 @@
+import {ENV_NAME, TEST_SERVICE_NAME} from '../../environment'
+import Automator from '../../util/cli-automator'
+
+export default function () {
+
+  this.When(/I launch a service from the CLI prompt$/, async function () {
+    const cmd = Automator.spawn(['-e', ENV_NAME, 'launch'])
+    await cmd.waitForLaunch()
+
+    // key commands to select the service from the list
+    await cmd.send('space')
+    await cmd.send('enter')
+
+    await cmd.waitForDone()
+  })
+
+  this.When(/I launch a service from the CLI directly$/, async function () {
+    const cmd = Automator.spawn(['-e', ENV_NAME, 'launch', TEST_SERVICE_NAME])
+    await cmd.waitForDone()
+  })
+
+}
---
+++ test/integration/util/cli-automator.js
@@ -0,0 +1,94 @@
+import path from 'path'
+import pty from 'pty.js'
+import stripAnsi from 'strip-ansi'
+
+process.stdin.setEncoding('utf8')
+if (typeof process.stdin.setRawMode === 'function') {
+  process.stdin.setRawMode(true)
+}
+
+// from http://stackoverflow.com/questions/17470554/how-to-capture-the-arrow-keys-in-node-js
+const keyCodes = {
+  'up': '\u001b[A',
+  'down': '\u001b[B',
+  'right': '\u001b[C',
+  'left': '\u001b[D',
+  'enter': '\n',
+  'space': ' ',
+}
+
+const SYNTHETIC_DELAY_MS = 150
+
+export const NAVY_BIN = path.join(__dirname, '../../../bin/navy.js')
+
+export default class Automator {
+
+  static spawn(args, opts = {}) {
+    const automator = new this(args, opts)
+    automator.spawn()
+    return automator
+  }
+
+  constructor(args, opts) {
+    this.args = args
+    this.opts = opts
+  }
+
+  spawn() {
+    console.log()
+    console.log(' >', NAVY_BIN, this.args.join(' '))
+    console.log()
+
+    this.output = ''
+
+    this.term = pty.spawn(NAVY_BIN, this.args, {
+      name: 'xterm-color',
+      cols: 130,
+      rows: 30,
+      cwd: this.opts.cwd || process.env.HOME,
+      env: this.opts.env || process.env,
+    })
+
+    this.term.pipe(process.stdout)
+    process.stdin.pipe(this.term)
+
+    this.term.on('data', data => this.output += data.toString())
+
+    this.term.on('exit', function () {
+      console.log()
+      console.log(' - Exited')
+      console.log()
+    })
+  }
+
+  send(keyCode) {
+    // synthetic delay
+    return new Promise(resolve =>
+      setTimeout(() => {
+        this.term.write(keyCodes[keyCode] || keyCode)
+        resolve()
+      }, SYNTHETIC_DELAY_MS)
+    )
+  }
+
+  waitForDone() {
+    return new Promise(resolve =>
+      this.term.once('exit', () => resolve(stripAnsi(this.output)))
+    )
+  }
+
+  waitForLaunch() {
+    return new Promise(resolve =>
+      this.term.once('data', resolve)
+    )
+  }
+
+  getOutput() {
+    return stripAnsi(this.output)
+  }
+
+  getRawOutput() {
+    return this.output
+  }
+
+}
