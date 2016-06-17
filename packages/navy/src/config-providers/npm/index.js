3c187ea1000c10df05e9c05b126f09e5a21a8aec
Jamie Greeff <jamie@greeff.me>
2016-06-17 15:16:00 +0100

    Improve NPM config provider, don't install packages globally

--- packages/navy/src/config-providers/npm/index.js
+++ packages/navy/src/config-providers/npm/index.js
@@ -2,32 +2,35 @@
 
 import path from 'path'
 import {execSync} from 'child_process'
+import bluebird from 'bluebird'
 import {Navy} from '../../navy'
 import fs from '../../util/fs'
+import {pathToNavyRoot} from '../../navy/state'
 
 import type {ConfigProvider} from '../../config-provider'
 import type {State} from '../../navy'
 
-let _cachedNpmPrefix = null
+const mkdirp = bluebird.promisify(require('mkdirp'))
+
+const npmContext = path.join(pathToNavyRoot(), 'npm')
+const nodeModulesPath = path.join(npmContext, 'node_modules')
 
 async function tryAndInstall(pkgName) {
+  await mkdirp(nodeModulesPath)
+
   try {
     // $FlowIgnore some weird bug with execSync
-    execSync(`npm info ${pkgName} name`, { stdio: 'inherit' })
+    execSync(`npm info ${pkgName} name`, { stdio: 'ignore' })
   } catch (ex) {
     throw new Error(`Package "${pkgName}" not found or unreachable`)
   }
 
   // $FlowIgnore some weird bug with execSync
-  execSync(`npm i -g ${pkgName}`, { stdio: 'inherit' })
+  execSync(`npm i ${pkgName}`, { stdio: 'inherit', cwd: npmContext })
 }
 
 async function pathToModule(pkgName) {
-  if (!_cachedNpmPrefix) {
-    _cachedNpmPrefix = execSync('npm config get prefix').toString().trim()
-  }
-
-  return path.join(_cachedNpmPrefix, '/lib/node_modules/', pkgName)
+  return path.join(nodeModulesPath, pkgName)
 }
 
 export default function createNpmConfigProvider(navy: Navy): ConfigProvider {
@@ -43,13 +46,9 @@ export default function createNpmConfigProvider(navy: Navy): ConfigProvider {
         throw new Error('NPM config provider requires an NPM package')
       }
 
-      if (!_cachedNpmPrefix) {
-        _cachedNpmPrefix = execSync('npm config get prefix').toString().trim()
-      }
-
       const navyPath = await pathToModule(envState.npmPackage)
 
-      await fs.accessAsync(navyPath) // see if path exists
+      fs.accessSync(navyPath) // see if path exists
 
       return navyPath
     },
