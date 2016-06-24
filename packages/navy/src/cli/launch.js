6366d9f2cd9dc95d0d7d96628860c1492a4d388d
Jamie Greeff <jamie@greeff.me>
2016-06-24 12:21:45 +0100

    Allow specifying default services for navy launch, #24

--- packages/navy/src/cli/launch.js
+++ packages/navy/src/cli/launch.js
@@ -6,17 +6,23 @@ import {startDriverLogging, stopDriverLogging} from '../driver-logging'
 
 export default async function (services: Array<string>, opts: Object): Promise<void> {
   const env = getNavy(opts.navy)
+  const navyFile = (await env.getNavyFile()) || {}
 
   await env.ensurePluginsLoaded()
 
   const serviceNames = await env.getAvailableServiceNames()
   const launchedServiceNames = await env.getLaunchedServiceNames()
 
+  const selectedServiceNames = launchedServiceNames.length > 0
+    ? launchedServiceNames
+    : navyFile.launchDefaults || []
+
   if (!services || services.length === 0) {
     const choices = serviceNames
+      .sort(name => selectedServiceNames.indexOf(name) === -1 ? 100 : -1)
       .map(name => ({
         name,
-        checked: launchedServiceNames.indexOf(name) !== -1,
+        checked: selectedServiceNames.indexOf(name) !== -1,
       }))
 
     const { services: selectedServices } = await inquirer.prompt([{
