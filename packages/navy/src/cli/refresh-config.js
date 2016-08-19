9d242d78bcd1bd72535248b0a1cef6cc768c0426
Jamie Greeff <jamie@greeff.me>
2016-08-19 09:58:06 +0100

    Ensure services are up to date after refreshing config

--- packages/navy/src/cli/refresh-config.js
+++ packages/navy/src/cli/refresh-config.js
@@ -3,6 +3,7 @@
 import chalk from 'chalk'
 import invariant from 'invariant'
 import {getNavy} from '../'
+import {startDriverLogging, stopDriverLogging} from '../driver-logging'
 
 export default async function (opts: Object): Promise<void> {
   const navy = getNavy(opts.navy)
@@ -11,9 +12,11 @@ export default async function (opts: Object): Promise<void> {
 
   invariant(configProvider, 'NO_CONFIG_PROVIDER', navy.name)
 
-  if (!await configProvider.refreshConfig()) {
-    return console.log(chalk.dim('Nothing to do'))
-  }
+  await configProvider.refreshConfig()
+
+  startDriverLogging('Ensuring services are up to date...')
+  await navy.reconfigure()
+  stopDriverLogging()
 
   console.log(chalk.green(`Config refreshed for Navy "${opts.navy}"`))
 }
