f06074b9aa3590afc3f863cedfd846cc52b849dd
Jamie Greeff <jamie@greeff.me>
2016-08-25 09:57:54 +0100

    Improve website build tools and fix client js not working in Firefox

---
+++ website/Gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+const {execSync} = require('child_process')
+
+const exec = (cmd) => execSync(cmd, { stdio: 'inherit' })
+
+const paths = {
+  styles: 'styles/**/*',
+  html: ['../docs/**/*', '../packages/navy/src/**/*', 'layouts/**/*', 'content/**/*'],
+  resources: ['../docs/**/resources/*', 'public/**/*'],
+  clientJs: 'client-js/**/*',
+}
+
+gulp.task('styles', () => {
+  exec('./node_modules/.bin/lessc --clean-css="--s1 --advanced --compatibility=ie8" styles/main.less build/styles.css')
+})
+
+gulp.task('resources', () => {
+  gulp.src('public/**/*')
+  .pipe(gulp.dest('build/'))
+
+  gulp.src('../docs/**/resources/*')
+  .pipe(gulp.dest('build/docs'))
+})
+
+gulp.task('client-js', () => {
+  exec('../node_modules/.bin/babel --minified -d build/js client-js')
+})
+
+gulp.task('html', () => {
+  exec('../node_modules/.bin/babel-node .')
+})
+
+gulp.task('build', ['styles', 'resources', 'client-js', 'html'])
+
+gulp.task('watch', ['build'], () => {
+  gulp.watch(paths.styles, ['styles'])
+  gulp.watch(paths.resources, ['resources'])
+  gulp.watch(paths.clientJs, ['client-js'])
+  gulp.watch(paths.html, ['html'])
+})
--- website/client-js/api-doc.js
+++ website/client-js/api-doc.js
@@ -3,8 +3,8 @@ const docNav = document.querySelector('.api-doc-nav')
 
 docNav.insertAdjacentHTML('beforeend', `
   ${headings.map(headingElem =>
-    `<li class="doc-${headingElem.localName}" data-key="${headingElem.innerText}">
-       <a href="#${headingElem.innerText.toLowerCase()}">${headingElem.innerText}</a>
+    `<li class="doc-${headingElem.localName}" data-key="${headingElem.textContent}">
+       <a href="#${headingElem.textContent.toLowerCase()}">${headingElem.textContent}</a>
      </li>`
   ).join('\n')}
 `)
@@ -15,7 +15,7 @@ window.onscroll = function () {
   const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop
 
   for (const heading of headings) {
-    headingPositions[heading.innerText] = heading.offsetTop
+    headingPositions[heading.textContent] = heading.offsetTop
   }
 
   for (const heading in headingPositions) {
