8d455015a6a8783fa6d91ce94a5e9b113dc9a71c
James Tancock <james.tancock@momentumft.co.uk>
2017-02-22 10:14:32 +0000

    Handle errors when traversing the node_modules dir to stop EventEmitter throwing an exception

--- packages/navy-plugin-nodejs/src/hooks/rewrite-linked-node-modules.js
+++ packages/navy-plugin-nodejs/src/hooks/rewrite-linked-node-modules.js
@@ -1,9 +1,16 @@
 import findit from 'findit'
 import path from 'path'
 import fs from 'fs'
+import os from 'os'
+
+import {name as pluginName} from '../../package.json'
 
 export default () => {
   const finder = findit(path.join(process.cwd(), 'node_modules'))
+
+  finder.on('error', err => {
+    console.error(`${pluginName} failed to traverse your node_modules directory:`, os.EOL, err.toString())
+  })
 
   finder.on('link', link => {
     if (link.indexOf('.bin') !== -1) return
