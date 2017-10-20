57778d46dc6ff46cc6719992179cc6984e095712
James Tancock <james.tancock@candide.eu>
2017-10-20 16:00:17 +0100

    Fix website builder
    The api reference docs were missing because the documentation
    package had starting using Promises natively, so our promisification
    was silently breaking the Gulp task.

--- website/api-docs.js
+++ website/api-docs.js
@@ -1,15 +1,14 @@
-import bluebird from 'bluebird'
+import documentation from 'documentation'
 
-const documentation = bluebird.promisifyAll(require('documentation'))
-const docFormats = bluebird.promisifyAll(require('documentation').formats)
+const docFormats = documentation.formats
 
 export default async function () {
-  const docs = await documentation.buildAsync([
+  const docs = await documentation.build([
     '../packages/navy/src/navy/index.js',
     '../packages/navy/src/navy/state.js',
   ], { access: ['public'] })
 
-  const docsMarkdown = await docFormats.mdAsync(docs, {})
+  const docsMarkdown = await docFormats.md(docs, {})
 
   return [{
     path: 'index.md',
