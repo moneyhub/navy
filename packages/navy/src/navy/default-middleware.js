df7b5b6d8f2977c0342776615868762718b1b921
Thom Wright <thom.wright@candide.eu>
2017-10-20 10:13:26 +0100

    Allow setting environment variables using state
    This change allows me to do `navy.initialise(state)` where state
    includes environment variables to set for a service.
    
    This is very useful for e.g. tests, where we want to run a service with
    different environment variables from the default.

--- packages/navy/src/navy/default-middleware.js
+++ packages/navy/src/navy/default-middleware.js
@@ -2,6 +2,7 @@ import developMiddleware from '../middleware/develop'
 import tagOverrideMiddleware from '../middleware/tag-override'
 import portOverrideMiddleware from '../middleware/port-override'
 import addVirtualHostsMiddleware from '../middleware/add-virtual-hosts'
+import setEnvironmentVariables from "../middleware/set-env-vars"
 
 import type {Navy} from './'
 
@@ -9,5 +10,6 @@ export default (navy: Navy) => ([
   developMiddleware,
   tagOverrideMiddleware,
   portOverrideMiddleware,
+  setEnvironmentVariables,
   addVirtualHostsMiddleware(navy),
 ])
