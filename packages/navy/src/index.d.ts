95c2e7d013351e64f0aaf585b885ceb84549b107
Thom Wright <thom.wright@candide.eu>
2018-05-01 14:49:18 +0100

    Add more TS types

--- packages/navy/src/index.d.ts
+++ packages/navy/src/index.d.ts
@@ -4,11 +4,51 @@ declare module "navy" {
     path?: string
   }
 
+  export enum ServiceStatus {
+    RUNNING = "running",
+    EXITED = "exited",
+  }
+
+  export type Service = {
+    id: string
+    name: string
+    image: string
+    status: ServiceStatus
+    raw?: Object
+  }
+
+  export type ServiceList = Array<Service>
+
+  export interface ServiceHealthData {
+    service: string
+    health: "healthy" | "unhealthy" | "starting"
+  }
+
+  export interface LaunchOptions {
+    noDeps?: boolean
+  }
+
   export interface Navy {
     destroy: () => Promise<void>
+    externalIP: () => Promise<string>
     initialise: (opts: State) => Promise<void>
-    launch: (services: string[]) => Promise<void>
-    port: (service: string, privatePort: number) => Promise<number | undefined>
+    isInitialised: () => Promise<boolean>
+    launch: (
+      services?: Array<string> | null,
+      opts?: LaunchOptions,
+    ) => Promise<void>
+    port: (
+      service: string,
+      privatePort: number,
+      index?: number,
+    ) => Promise<number | undefined>
+    spawnLogStream: (services: Array<string>) => Promise<void>
+    url: (service: string) => Promise<string>
+    waitForHealthy: (
+      services?: Array<string> | null,
+      progressCallback?: (healthData: Array<ServiceHealthData>) => void,
+    ) => Promise<boolean>
+    ps(): Promise<ServiceList>
   }
 
   export function getNavy(name: string): Navy
