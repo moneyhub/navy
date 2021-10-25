640c1d9f0e70709f4dbb28dcf79dc09d7b095fd0
jonbride <12625934+jonbride@users.noreply.github.com>
2021-10-25 15:45:01 +0100

    Fix how integration tests get Navy services
    They were expecting a services object on the Navy object,
    but this is not part of the API
    
    Use the official Navy.ps() to get services instead
    
    This also removes forced package resolutions,
    as they're no longer required

--- test/integration/features/step_definitions/start-stop-cli.js
+++ test/integration/features/step_definitions/start-stop-cli.js
@@ -1,9 +1,12 @@
-import {expect} from 'chai'
+import {expect, use} from 'chai'
 import {TEST_SERVICE_NAME} from '../../environment'
 import Automator from '../../util/cli-automator'
 import {Service} from '../../../../packages/navy/lib'
 
 import {Then, When} from '@cucumber/cucumber'
+
+use(require('chai-like'))
+use(require('chai-things'))
 
 When(/I stop the service via the CLI$/, async function () {
   this.result = await Automator.spawn(['stop', TEST_SERVICE_NAME]).waitForDone()
@@ -18,9 +21,12 @@ When(/I start the navy via the CLI$/, async function () {
 })
 
 Then(/I should see that just that service is running$/, async function () {
-  await expect(this.navy).to.have.services([
+  const services = await this.navy.ps()
+
+  expect(services).to.have.length(1)
+  expect(services).to.to.be.an('array').that.contains.something.like(
     { name: TEST_SERVICE_NAME, status: Service.Status.RUNNING },
-  ])
+  )
 })
 
 Then(/I should see that the navy hasn't been initialised$/, async function () {
